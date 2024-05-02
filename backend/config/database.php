885d09788f9de9024a9a851f1cecba3f8b72b1b2
LUIS GUSTAVO RODRIGUES BENVINDO <luis.grbenvindo@senacsp.edu.br>
2024-05-02 20:44:22 -0300

    DAO criado

--- backend/config/database.php
+++ backend/config/database.php
@@ -12,7 +12,7 @@
             self::$instance = new PDO("mysql:host=$host;dbname=$dbname",$username, $password);
             self::$instance->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
         }
-        return self::$instance
+        return self::$instance;
     }
  }
 
