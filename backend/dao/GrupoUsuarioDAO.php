fa23fea75fd9056f131f2e13f7ad8afa706240a9
LUIS GUSTAVO RODRIGUES BENVINDO <luis.grbenvindo@senacsp.edu.br>
2024-05-06 22:15:57 -0300

    Exercicios inicializados

--- backend/dao/GrupoUsuarioDAO.php
+++ backend/dao/GrupoUsuarioDAO.php
@@ -1,20 +1,23 @@
 <?php
 
 require_once 'config/Database.php';
-require_once 'entity/Usario.php';
+require_once 'entity/Grupo.php';
 require_once 'BaseDAO.php';
 
-class UsuarioDAO implements BaseDAO {
+class GrupoDAO implements BaseDAO
+{
     private $db;
 
-    public function __construct() {
+    public function __construct()
+    {
         $this->db = Database::getInstance();
     }
 
-    public function getById($id) {
+    public function getById($id)
+    {
         try {
             // Preparar a consulta SQL
-            $sql = "SELECT * FROM Usuario WHERE Id = :id";
+            $sql = "SELECT * FROM grupousuario WHERE Id = :id";
 
             // Preparar a instrução
             $stmt = $this->db->prepare($sql);
@@ -26,77 +29,82 @@ class UsuarioDAO implements BaseDAO {
             $stmt->execute();
 
             // Obtem o usuario encontrado;
-            $usuario = $stmt->fetch(PDO::FETCH_ASSOC);
+            $grupo = $stmt->fetch(PDO::FETCH_ASSOC);
 
             // Retorna o usuário encontrado
-            return $usuario ? 
-                new Usuario($usuario['Id'],
-                            $usuario['NomeUsuario'], 
-                            $usuario['Senha'], 
-                            $usuario['Email'], 
-                            $usuario['GrupoUsuarioID'],
-                            $usuario['Ativo'],
-                            $usuario['DataCriacao'],
-                            $usuario['DataAtualizacao']) 
+            return $grupo ?
+                new Usuario(
+                    $grupo['Id'],
+                    $grupo['Nome'],
+                    $grupo['Descricao'],
+                    $grupo['DataCriacao'],
+                    $grupo['DataAtualizacao'],
+                    $grupo['Ativo']
+                )
                 : null;
         } catch (PDOException $e) {
             return null;
         }
     }
 
-    public function getAll() {
+    public function getAll()
+    {
         try {
             // Preparar a consulta SQL
-            $sql = "SELECT * FROM Usuario";
-    
+            $sql = "SELECT * FROM grupousuario";
+
             // Preparar a instrução
             $stmt = $this->db->prepare($sql);
-    
+
             // Executar a instrução
             $stmt->execute();
-    
+
             // Obter todos os usuários encontrados
-            $usuarios = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    
+            $grupo = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
             // Retornar os usuários encontrados
-            return array_map(function ($usuario) {
-                return new Usuario($usuario['Id'], 
-                            $usuario['NomeUsuario'], 
-                            $usuario['Senha'], 
-                            $usuario['Email'], 
-                            $usuario['GrupoUsuarioID'], 
-                            $usuario['Ativo'], 
-                            $usuario['DataCriacao'], 
-                            $usuario['DataAtualizacao']);
-            }, $usuarios);
+            return array_map(function ($grupo) {
+                return new GrupoUsuario(
+                    $grupo['Id'],
+                    $grupo['Nome'],
+                    $grupo['descricao'],
+                    $grupo['DataCriacao'],
+                    $grupo['DataAtualizacao'],
+                    $grupo['UsuarioAtualizacao'],
+                    $grupo['Ativo']
+                );
+            }, $grupo);
         } catch (PDOException $e) {
             return [];
         }
     }
 
 
-    public function create($usuario) {
+    public function create($grupo)
+    {
         try {
             // Preparar a consulta SQL
-            $sql = "INSERT INTO Usuario( NomeUsuario , Senha , Email , GrupoUsuarioID , Ativo , DataCriacao , DataAtualizacao , UsuarioAtualizacao )
-                    VALUES(:nomeUsuario, :senha, :email, :grupoUsuarioID, :ativo, current_timestamp(),current_timestamp(),null)";
+            $sql = "INSERT INTO grupousuario( nome , descricao, usuarioatualizacao, Ativo, dataCriacao , dataAtualizacao)
+                    VALUES(:nome, :descricao,:usuarioatualizacao,:ativo, current_timestamp(), current_timestamp())";
 
             // Preparar a instrução
             $stmt = $this->db->prepare($sql);
 
-             // Bind parameters by reference
-            $nomeUsuario = $usuario->getNomeUsuario();
-            $senha = $usuario->getSenha();
-            $email = $usuario->getEmail();
-            $grupoUsuarioID = $usuario->getGrupoUsuarioId();
-            $ativo = $usuario->getAtivo();
+            // Bind parameters by reference
+            $nomeGrupo = $grupo->getNome();
+            $descricao = $grupo->getDescricao();
+            $dataCriacao = $grupo->getDataCriacao();
+            $dataAtualizacao = $grupo->getDataAtulizacao();
+            $usuarioAtualizacao = $grupo->getUsuarioAtualizacao();
+            $ativo = $grupo->getAtivo();
 
-            $stmt->bindParam(':nomeUsuario', $nomeUsuario);
-            $stmt->bindParam(':senha', $senha);
-            $stmt->bindParam(':email', $email);
-            $stmt->bindParam(':grupoUsuarioID', $grupoUsuarioID);
+            $stmt->bindParam(':nome', $nomeGrupo);
+            $stmt->bindParam(':descricao', $descricao);
+            $stmt->bindParam(':dataCriacao', $dataCriacao);
+            $stmt->bindParam(':dataAtualizacao', $dataAtualizacao);
+            $stmt->bindParam(':usuarioAtualizacao', $usuarioAtualizacao);
             $stmt->bindParam(':ativo', $ativo);
-            
+
             // Executar a instrução
             $stmt->execute();
 
@@ -108,13 +116,14 @@ class UsuarioDAO implements BaseDAO {
         }
     }
 
-    public function update($usuario) {
+    public function update($usuario)
+    {
         try {
             // Verifico se o usuário existe no banco de dados
             $existingUser = $this->getById($usuario->getId());
 
-            if(!$existingUser) {
-                return false;// Retorna falso se o usuário não existir
+            if (!$existingUser) {
+                return false; // Retorna falso se o usuário não existir
             }
 
             $sql = "UPDATE Usuario SET NomeUsuario = :nomeUsuario, Senha = :senha, Email = :email,
@@ -135,23 +144,23 @@ class UsuarioDAO implements BaseDAO {
             $stmt->bindParam(':email', $email);
             $stmt->bindParam(':grupoUsuarioID', $grupoUsuarioID);
             $stmt->bindParam(':ativo', $ativo);
-            
+
 
             $stmt->execute();
 
             return true;
-
         } catch (PDOException $e) {
             // TO-DO: implementar log
             return false;
         }
     }
 
-    public function delete($id) {
+    public function delete($id)
+    {
         try {
             $sql = "DELETE FROM Usuario WHERE Id = :id";
             $stmt = $this->db->prepare($sql);
-            
+
             $stmt->bindParam(':id', $id);
             $stmt->execute();
 
@@ -161,5 +170,3 @@ class UsuarioDAO implements BaseDAO {
         }
     }
 }
-
-?>
--- backend/index.php
+++ backend/index.php
@@ -1,9 +1,34 @@
 <?php
-    require_once("template/header.php")
+require_once("template/header.php");
+// require_once 'dao/UsarioDAO.php';
+// require_once 'entity/Usario.php';
+require_once 'dao/GrupoUsuarioDAO.php';
+require_once 'entity/grupo.php';
+
+// $usuarioDao = new UsuarioDAO();
+// print_r($usuarioDao->getAll());
+
+// $novoUsuario = new Usuario(null, "novo usuario B", "1234aerqe", "novouser@mail.com", null, 1);
+// echo $novoUsuario->getNomeUsuario();
+
+
+// $usuarioDao->create($novoUsuario);
+// $novoUsuario = new Usuario(2, "Clayton", "1234aerqe", "novouser@mail.com", null, 1);
+// $usuarioDao->update($no);
+// $grupo = new GrupoDAO();
+// print_r($grupo->getAll());
+
+$grupodao = new GrupoDAO();
+$novoGrupo = new GrupoUsuario(null, "teste", "teste", null, 1);
+$grupodao->create($novoGrupo);
+
+ 
+
+
 ?>
-    <h1>Olá Sistema vendas Body</h1>
-    
+<h1>Olá Sistema Vendas Body</h1>
 </body>
+
 <?php
-    require_once("template/footer.php")
+require_once("template/footer.php");
 ?>
