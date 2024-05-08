06672a396c40b33ec3337f5a87acc4a3c2141dc4
LUIS GUSTAVO RODRIGUES BENVINDO <luis.grbenvindo@senacsp.edu.br>
2024-05-07 22:13:03 -0300

    Atualizando o front end

---
+++ backend/login.php
@@ -0,0 +1,7 @@
+<?php
+require_once("template/header.php");
+?>
+<h1>PLACEHOLDER DA PAGINA DE LOGIN</h1>
+<?php
+require_once("template/footer.php");
+?>
---
+++ backend/sobre.php
@@ -0,0 +1,7 @@
+<?php
+require_once("template/header.php");
+?>
+<h1>PAGINA DE ESTUDO SENAC 2024</h1>
+<?php
+require_once("template/footer.php");
+?>
