9a3066763903c2a25eb5728e35674f12b2380966
LUIS GUSTAVO RODRIGUES BENVINDO <luis.grbenvindo@senacsp.edu.br>
2024-05-08 19:56:20 -0300

    Pagina de login estatica

--- backend/index.php
+++ backend/index.php
@@ -2,7 +2,7 @@
 require_once("template/header.php");
 ?>
 <div class="container">
-    <div class="row row-cols-1 rows-cols-md-3 g-4">
+    <div class="row row-cols-1 row-cols-md-3 g-4">
         <?php for ($i = 0; $i < 6; $i++) : ?>
             <div class="col">
                 <div class="card">
@@ -10,12 +10,13 @@ require_once("template/header.php");
                     <div class="card-body">
                         <h5 class="card-title">Album - Eternal Sunshine</h5>
                         <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <a href="login.php" class="btn btn-primary">Go somewhere</a>
                     </div>
                 </div>
             </div>
+
+        <?php endfor; ?>
     </div>
-<?php endfor; ?>
 </div>
 
 <?php
--- backend/login.php
+++ backend/login.php
@@ -1,7 +1,46 @@
 <?php
 require_once("template/header.php");
 ?>
-<h1>PLACEHOLDER DA PAGINA DE LOGIN</h1>
+<div class="row justify-content-center">
+    <div class="col-md-6">
+        <h2>LOGIN</h2>
+        <form action="authservice.php" method="post">
+            <input type="hidden" name="type" value="login">
+            <div class="mb-3">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control" id="email" required>
+            </div>
+            <div class="mb-3">
+                <label for="password" class="form-label">Senha</label>
+                <input type="password" class="form-control" id="password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">ENTRAR</button>
+        </form>
+    </div>
+    <div class="col-md-6">
+    <h2>CADASTRO</h2>
+        <form action="authservice.php" method="post">
+            <input type="hidden" name="type" value="login">
+            <div class="mb-3">
+                <label for="new_nome" class="form-label">Nome</label>
+                <input type="new_nome" class="form-control" id="new_nome" required>
+            </div>
+            <div class="mb-3">
+                <label for="new_email" class="form-label">Email</label>
+                <input type="new_email" class="form-control" id="new_email" required>
+            </div>
+            <div class="mb-3">
+                <label for="new_password" class="form-label">Senha</label>
+                <input type="new_password" class="form-control" id="new_password" required>
+            </div>
+            <div class="mb-3">
+                <label for="confirm_password" class="form-label">confirmar senha</label>
+                <input type="confirm_password" class="form-control" id="confirm_password" required>
+            </div>
+            <button type="submit" class="btn btn-primary">ENTRAR</button>
+        </form>
+    </div>
+</div>
 <?php
 require_once("template/footer.php");
 ?>
